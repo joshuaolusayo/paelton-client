f93621de35b65aaf106923eb7f15a93360594ffb
Joshua Oyeleke <oyelekeoluwasayo@gmail.com>
2020-11-14 04:50:18 +0100

    Updated the message titles

--- src/Components/Homepage/Cards/Audios.js
+++ src/Components/Homepage/Cards/Audios.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import AOS from "aos";
 
 const Audios = () => {
+	useEffect(() => {
+		AOS.init({ duration: 600, once: true });
+	});
+
 	return (
-		<div className="container">
+		<div className="container audios">
 			<div className="row">
-				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5">
-					<div className="card bg-light shadow">
+				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5" data-aos="fade-up">
+					<div className="card bg-light shadow h-100">
 						<img
 							className="card-img-top"
 							src="/assets/understand_ye-the_kingdom_part_1.jpg"
@@ -19,12 +24,14 @@ const Audios = () => {
 								looking at its layout. The point of
 							</p>
 							<div className="d-flex justiy-content-between align-items-center">
-								<button className="btn bg-pry text-light dwn">
-									Download&nbsp;
-									<span>
-										<i className="fa fa-download"></i>
-									</span>
-								</button>
+								<a href="/audio.mp3" download="Understand ye the kingdom part 1">
+									<button className="btn bg-pry text-light dwn">
+										Download&nbsp;
+										<span>
+											<i className="fa fa-download"></i>
+										</span>
+									</button>
+								</a>
 								<audio controls className="audio">
 									<source src="/audio.mp3" type="audio/mpeg" />
 									Your browser does not support the audio element.
@@ -33,8 +40,8 @@ const Audios = () => {
 						</div>
 					</div>
 				</div>
-				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5">
-					<div className="card bg-light shadow">
+				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5" data-aos="fade-up">
+					<div className="card bg-light shadow h-100">
 						<img
 							className="card-img-top"
 							src="/assets/understand_ye-the_kingdom_part_2.jpg"
@@ -48,12 +55,14 @@ const Audios = () => {
 								looking at its layout. The point of
 							</p>
 							<div className="d-flex justiy-content-between align-items-center">
-								<button className="btn bg-pry text-light dwn">
-									Download&nbsp;
-									<span>
-										<i className="fa fa-download"></i>
-									</span>
-								</button>
+								<a href="/audio.mp3" download="Understand ye the kingdom part 2">
+									<button className="btn bg-pry text-light dwn">
+										Download&nbsp;
+										<span>
+											<i className="fa fa-download"></i>
+										</span>
+									</button>
+								</a>
 								<audio controls className="audio">
 									<source src="/audio.mp3" type="audio/mpeg" />
 									Your browser does not support the audio element.
@@ -62,12 +71,12 @@ const Audios = () => {
 						</div>
 					</div>
 				</div>
-				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5">
-					<div className="card bg-light shadow">
+				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5" data-aos="fade-up">
+					<div className="card bg-light shadow h-100">
 						<img
 							className="card-img-top"
 							src="/assets/My little children, love not the world.jpg"
-							alt="Understand ye the kingdom part 1"
+							alt="My little children, love not the world"
 							loading="lazy"
 						/>
 						<div className="card-body p-4">
@@ -77,12 +86,14 @@ const Audios = () => {
 								looking at its layout. The point of
 							</p>
 							<div className="d-flex justiy-content-between align-items-center">
-								<button className="btn bg-pry text-light dwn">
-									Download&nbsp;
-									<span>
-										<i className="fa fa-download"></i>
-									</span>
-								</button>
+								<a href="/audio.mp3" download="My little children, love not the world">
+									<button className="btn bg-pry text-light dwn">
+										Download&nbsp;
+										<span>
+											<i className="fa fa-download"></i>
+										</span>
+									</button>
+								</a>
 								<audio controls className="audio">
 									<source src="/audio.mp3" type="audio/mpeg" />
 									Your browser does not support the audio element.
@@ -91,12 +102,12 @@ const Audios = () => {
 						</div>
 					</div>
 				</div>
-				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5">
-					<div className="card bg-light shadow">
+				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5" data-aos="fade-up">
+					<div className="card bg-light shadow h-100">
 						<img
 							className="card-img-top"
 							src="/assets/The coming kingdom and its glory. Part 1.jpg"
-							alt="Understand ye the kingdom part 1"
+							alt="The coming kingdom and its glory. Part 1"
 							loading="lazy"
 						/>
 						<div className="card-body p-4">
@@ -106,12 +117,14 @@ const Audios = () => {
 								looking at its layout. The point of
 							</p>
 							<div className="d-flex justiy-content-between align-items-center">
-								<button className="btn bg-pry text-light dwn">
-									Download&nbsp;
-									<span>
-										<i className="fa fa-download"></i>
-									</span>
-								</button>
+								<a href="/audio.mp3" download="The coming kingdom and its glory. Part 1">
+									<button className="btn bg-pry text-light dwn">
+										Download&nbsp;
+										<span>
+											<i className="fa fa-download"></i>
+										</span>
+									</button>
+								</a>
 								<audio controls className="audio">
 									<source src="/audio.mp3" type="audio/mpeg" />
 									Your browser does not support the audio element.
@@ -120,12 +133,12 @@ const Audios = () => {
 						</div>
 					</div>
 				</div>
-				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5">
-					<div className="card bg-light shadow">
+				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5" data-aos="fade-up">
+					<div className="card bg-light shadow h-100">
 						<img
 							className="card-img-top"
 							src="/assets/The coming kingdom and its glory. Part 2.jpg"
-							alt="Understand ye the kingdom part 1"
+							alt="The coming kingdom and its glory. Part 2"
 							loading="lazy"
 						/>
 						<div className="card-body p-4">
@@ -135,12 +148,14 @@ const Audios = () => {
 								looking at its layout. The point of
 							</p>
 							<div className="d-flex justiy-content-between align-items-center">
-								<button className="btn bg-pry text-light dwn">
-									Download&nbsp;
-									<span>
-										<i className="fa fa-download"></i>
-									</span>
-								</button>
+								<a href="/audio.mp3" download="The coming kingdom and its glory. Part 1">
+									<button className="btn bg-pry text-light dwn">
+										Download&nbsp;
+										<span>
+											<i className="fa fa-download"></i>
+										</span>
+									</button>
+								</a>
 								<audio controls className="audio">
 									<source src="/audio.mp3" type="audio/mpeg" />
 									Your browser does not support the audio element.
@@ -149,12 +164,12 @@ const Audios = () => {
 						</div>
 					</div>
 				</div>
-				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5">
-					<div className="card bg-light shadow">
+				<div className="col-md-6 col-lg-4 mb-4 mb-lg-5" data-aos="fade-up">
+					<div className="card bg-light shadow h-100">
 						<img
 							className="card-img-top"
 							src="/assets/What meant these sayings of the Lord.jpg"
-							alt="Understand ye the kingdom part 1"
+							alt="What meant these sayings of the Lord?"
 							loading="lazy"
 						/>
 						<div className="card-body p-4">
@@ -164,12 +179,14 @@ const Audios = () => {
 								looking at its layout. The point of
 							</p>
 							<div className="d-flex justiy-content-between align-items-center">
-								<button className="btn bg-pry text-light dwn">
-									Download&nbsp;
-									<span>
-										<i className="fa fa-download"></i>
-									</span>
-								</button>
+								<a href="/audio.mp3" download="What meant these sayings of the Lord?">
+									<button className="btn bg-pry text-light dwn">
+										Download&nbsp;
+										<span>
+											<i className="fa fa-download"></i>
+										</span>
+									</button>
+								</a>
 								<audio controls className="audio">
 									<source src="/audio.mp3" type="audio/mpeg" />
 									Your browser does not support the audio element.
